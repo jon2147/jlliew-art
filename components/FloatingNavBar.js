6ea0ceaddfbcd3ca6b7523d7364b5957cc68fc70
Jon Liew <jon.l2147@hotmail.com>
2020-12-05 20:58:53 +1100

    Change global background to black to avoid white flashing for footer images

--- components/FloatingNavBar.js
+++ components/FloatingNavBar.js
@@ -1,13 +1,16 @@
 
 import MenuIcon from '@material-ui/icons/Menu';
 import styles from './FloatingNavBar.module.css';
+import Link from 'next/link'
 
 export default function FloatingNavBar() {
     return <>
 
         <div className={styles.bar}>
             <MenuIcon className={styles.menuIcon} />
-            <span className={styles.homeLink}>Jonathan Liew</span>
+            <Link href="/">
+                <a className={styles.homeLink}>Jonathan Liew</a>
+            </Link>
         </div>
     </>
 }
